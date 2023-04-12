f09db36609f5eecbb4a3914a13e34aaf634a4676
Cole Zill <98670265+Cole-Z@users.noreply.github.com>
2023-04-11 21:57:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ import tiktoken
 import discord
 from discord.ext import commands
 
-TOKEN = "your bot token"
+TOKEN = "bot token"
 
-openai.api_key = 'your api key'
+openai.api_key = 'open ai key'
 
 keywords = {
-    "warzone": "You are a Z League support rep, we have Warzone 1, but Warzone 2 will be on Z League soon!",
+    "warzone": "Z League host Warzone 1(Caldera) events, we hope to release Warzone 2 events soon, stay tuned!",
     "verify": "You are an AI assistant specialized in customer support for Z League. Players will need to verify "
               "their ID through persona our third party partner. If they need further assistance, please have the "
               "reach out to us at contact@zleague.gg",
     "banned": "You are a Z League support rep, if a player is banned, please ensure they have filled out the ban "
-              "appeal form",
+              "appeal form.",
     "delete": "If a user wants to delete their Z League account, advise them to contact the support team to action "
-              "this at contact@zleague.gg "
+              "this at contact@zleague.gg.",
+    "dispute": "If a user has an issue with their score in an event, advise them that scores can sometimes take 45 "
+               "minutes to update, if they are still having issues after that time, please have them contact us by "
+               "email. "
     # Add more keywords and corresponding system messages here
 }
 
@@ -24,7 +27,11 @@ max_response_tokens = 250
 token_limit = 4096
 max_conversation_length = 5
 conversation = [{"role": "system", "content": "You are a helpful customer service rep for Z League, an online "
-                                              "tournament platform for e-sports and popular video games."}]
+                                              "tournament platform for e-sports and popular video games. We offer "
+                                              "tournaments for Apex Legends, Halo Infinite, League of Legends and "
+                                              "Warzone(Caldera). Please answer to the best of your ability and if you "
+                                              "dont know the answer, please ask the user to contact our support team "
+                                              "for further assistance at contact@zleague.gg."}]
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -60,10 +67,9 @@ async def process_message(user_input):
             break
 
     if not matched_keyword:
-        instruction = (
-            "You are customer support AI for Z League. Answer questions related to Z League and gaming only"
-            "If you cannot find the answer, please ask the user to contact us at contact@zleague.gg"
-        )
+        # Access the initial system message from the conversation
+        initial_system_message = conversation[0]["content"]
+        instruction = initial_system_message
 
     user_input_with_instruction = f"{instruction} {user_input}"
     conversation.append({"role": "user", "content": user_input_with_instruction})
